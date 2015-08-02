ef57fab6944230fde67c66c4636b15bddeae6b09
Turash Jurij <yrik.turash@gmail.com>
2015-08-02 19:35:42 +0300

    start project template

---
+++ ASP.NET MVC5 Bootstrap 3.3.1 LESS1/ASP.NET MVC5 Bootstrap 3.3.1 LESS1/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC5_Bootstrap3_3_1_LESS.Models;
+
+namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(TextAnalysis model = null)
+        {
+            if (model != null)
+            {
+                model = new TextAnalysis();
+                model.Text = "А й правда, крилатим ґрунту не треба.\n" +
+                            "Землі немає, то буде небо.\n\r" +
+
+                            "Немає поля, то буде воля.\n" +
+                            "Немає пари, то будуть хмари. \n\r" +
+
+                            "В цьому, напевно, правда пташина...\n" +
+                            "А як же людина? А що ж людина? \n\r" +
+
+                            "Живе на землі. Сама не літає.\n" +
+                            "А крила має. А крила має! \n\r" +
+
+                            "Вони, ті крила, не з пуху-пір*я,\n" +
+                            "А з правди, чесноти і довір*я. \n\r" +
+
+                            "У кого - з вірності у коханні.\n" +
+                            "У кого - з вічного поривання. \n\r" +
+
+                            "У кого - з щирості до роботи.\n" +
+                            "У кого - з щедрості на турботи. \n\r" +
+
+                            "У кого - з пісні, або з надії,\n" +
+                            "Або з поезії, або з мрії. \n\r" +
+
+                            "Людина нібито не літає...\n" +
+                            "А крила має. А крила має!";
+            }
+
+            return View(model);
+        }
+        public ActionResult Analysis(TextAnalysis model)
+        {
+            var txt = model.Text;
+            txt = Regex.Replace(txt, @"[^\w\s]", string.Empty);
+            txt = txt.Replace("\r\n", string.Empty);
+            var words = txt.Split();
+            var dictionary = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = dictionary[word] + 1;
+                }
+                else
+                {
+                    dictionary.Add(word, 1);
+                }
+            }
+            model.Counts = dictionary;
+            return View("Index", model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ASP.NET MVC5 Bootstrap 3.3.1 LESS1/ASP.NET MVC5 Bootstrap 3.3.1 LESS1/Models/IdentityModels.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ASP.NET_MVC5_Bootstrap3_3_1_LESS.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+
+    public class TextAnalysis
+    {
+        public string Text { get; set; }
+
+        public Dictionary<string, int> Counts { get; set; } 
+    }
+
+}
